2a9fe099177049e22014ba93ab4e6bc7a1893db6
Zaid Khan <brain@zaidkhan.me>
2017-08-15 01:47:55 -0400

    Added basic angular config and eslintrc for it.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.set('view engine', 'ejs');
-app.use(express.static(`${__dirname}/app`));
+app.use(express.static(`${__dirname}/mdx_frontend`));
 
 mongoose.connect('mongodb://localhost/memedex');
 mongoose.Promise = Promise;
