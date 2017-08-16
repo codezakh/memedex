7c91804efdb9dd19196d4b0e9e516fb3295b2af8
Zaid Khan <brain@zaidkhan.me>
2017-08-16 01:57:45 -0400

    Added mlab connection code.

--- server.js
+++ server.js
@@ -12,7 +12,16 @@ app.use(cors());
 app.set('view engine', 'ejs');
 app.use(express.static(`${__dirname}/mdx_frontend`));
 
-mongoose.connect('mongodb://localhost/memedex');
+if (process.env.MLAB_USERNAME) {
+  const username = process.env.MLAB_USERNAME;
+  const password = process.env.MLAB_PASSWORD;
+  const mongoUri = process.env.MLAB_CONNSTR;
+  const connectionString = `mongodb://${username}:${password}@${mongoUri}`;
+  mongoose.connect(connectionString);
+} else {
+  mongoose.connect('mongodb://localhost/memedex');
+}
+
 mongoose.Promise = Promise;
 app.use('/api', apiRoutes);
 
