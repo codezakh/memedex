4cd2db19355e6f293018c407a2838b6ca33cb87c
Zaid Khan <brain@zaidkhan.me>
2017-08-16 20:20:56 -0500

    Testing different ways of adding memes to a list.

--- mdx_frontend/controllers/landing.controller.js
+++ mdx_frontend/controllers/landing.controller.js
@@ -1,6 +1,14 @@
 (function () {
   angular.module('memeDex')
     .controller('LandingController', function ($log) {
+      var model = this;
+      model.listitems = [
+        { _id: 1, value: 'apple' },
+        { _id: 2, value: 'bapple' },
+        { _id: 3, value: 'capple' },
+        { _id: 4, value: 'dapple' },
+        { _id: 5, value: 'eapple' },
+      ];
       $log.info('landing controller invoked');
     });
 }());
